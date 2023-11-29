08bd7e4b1df9a1f1684c3b5eed185872f275cfed
DươngLondon <ndd050903@gmail.com>
2023-11-30 00:38:21 +0700

    BTL

---
+++ BaiTapLonSo1/BTL_Ridge.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Ridge
+import numpy as np
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+data = pd.read_csv("./Du_Lieu_1000_ban_ghi.csv")
+dt_Train,dt_Test = train_test_split(data,test_size=0.3,shuffle=True)
+
+X_train = dt_Train.iloc[:,[1,2,3,4]]
+y_train = dt_Train.iloc[:,4]
+X_test = dt_Test.iloc[:,[1,2,3,4]]
+y_test = dt_Test.iloc[:,4]
+print(f'X_train = \n{X_train}\nY_train = \n{y_train}\nX_test = {X_test}\nY_test = \n{y_test}')
+
+rg = Ridge(alpha=0.1, fit_intercept=True, copy_X=True, max_iter=None, tol=0.0001, solver='auto', positive=False, random_state=None)
+rg.fit(X_train,y_train)
+
+y_pred = rg.predict(X_test)
+y_test_array = np.array(y_test)
+print("Linear Regression")
+print("Coeffcient of determination: %.2f" % r2_score(y_test,y_pred))
+print("MAE: %.2f" % mean_absolute_error(y_test,y_pred))
+MSE = mean_squared_error(y_test,y_pred)
+print("MSE: %.2f" % MSE)
+RMSE = np.sqrt(MSE)
+print('RMSE: %.2f'%RMSE)
+print("Thuc te          Du doan              Chenh lech")
+for i in range(0,len(y_test_array)):
+    print("%.2f" % y_test_array[i]," ",y_pred[i]," ",abs(y_test_array[i]-y_pred[i]))
